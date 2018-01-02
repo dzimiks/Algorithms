76ee9e822ad71e98bbee4dbfea110264baa1df28
dzimiks <vana997@gmail.com>
2018-01-02 21:34:07 +0100

    Add directed BFS paths

--- src/algorithms/MainDigraphTraversals.java
+++ src/algorithms/MainDigraphTraversals.java
@@ -1,5 +1,6 @@
 package algorithms;
 
+import algorithms.digraphTraversals.DirectedBFSPaths;
 import algorithms.digraphTraversals.DirectedDFS;
 import algorithms.digraphTraversals.DirectedDFSPaths;
 import dataStructures.In;
@@ -11,16 +12,23 @@ public class MainDigraphTraversals {
 
 		In in = new In("tests/small-digraph.txt");
 		Digraph G = new Digraph(in);
-		int s = 2;
+		int s = 7;
 
 		System.out.println(G);
 	
 		// Directed DFS
-		System.out.println("Directed DFS\n");
+		System.out.println("============");
+		System.out.println("Directed DFS");
+		System.out.println("============\n");
+		
 		DirectedDFS directedDFS = new DirectedDFS(G, s);
 		
+		System.out.println("\n");
+		
 		// Directed DFS paths
-		System.out.println("\n\nDirected DFS paths\n");
+		System.out.println("==================");
+		System.out.println("Directed DFS paths");
+		System.out.println("==================\n");
 		DirectedDFSPaths directedDFSPaths = new DirectedDFSPaths(G, s);
 		
 		for (int v = 0; v < G.getV(); v++) {
@@ -39,5 +47,29 @@ public class MainDigraphTraversals {
             else 
             	System.out.printf("%d to %d:  not connected\n", s, v);
         }
+		
+		// Directed BFS paths
+		System.out.println("\n==================");
+		System.out.println("Directed BFS paths");
+		System.out.println("==================\n");
+		
+		DirectedBFSPaths directedBFSPaths = new DirectedBFSPaths(G, s);
+		
+		for (int v = 0; v < G.getV(); v++) {
+			if (directedBFSPaths.hasPathTo(v)) {
+				System.out.format("%d to %d: ", s, v);
+				
+				for (int x : directedBFSPaths.pathTo(v)) {
+					if (x == s)
+						System.out.print(x);
+					else
+						System.out.print(x + "-");
+				}
+				
+				System.out.println();
+			}
+			else 
+				System.out.format("%d to %d: not connected!\n", s, v);
+		}
 	}
 }
---
+++ src/algorithms/digraphTraversals/DirectedBFSPaths.java
@@ -0,0 +1,125 @@
+package algorithms.digraphTraversals;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+import dataStructures.graphs.Digraph;
+
+public class DirectedBFSPaths {
+
+	private static final int INFINITY = Integer.MAX_VALUE;
+    private boolean[] visited; 
+    private int[] edgeTo;     
+    private int[] distTo;     
+
+    public DirectedBFSPaths(Digraph G, int s) {
+        visited = new boolean[G.getV()];
+        distTo = new int[G.getV()];
+        edgeTo = new int[G.getV()];
+        
+        for (int v = 0; v < G.getV(); v++)
+            distTo[v] = INFINITY;
+        
+        validateVertex(s);
+        bfs(G, s);
+    }
+
+    public DirectedBFSPaths(Digraph G, Iterable<Integer> sources) {
+        visited = new boolean[G.getV()];
+        distTo = new int[G.getV()];
+        edgeTo = new int[G.getV()];
+        
+        for (int v = 0; v < G.getV(); v++)
+            distTo[v] = INFINITY;
+        
+        validateVertices(sources);
+        bfs(G, sources);
+    }
+
+    private void bfs(Digraph G, int s) {
+        LinkedList<Integer> q = new LinkedList<Integer>();
+        visited[s] = true;
+        distTo[s] = 0;
+        q.add(s);
+        
+        while (!q.isEmpty()) {
+            int v = q.poll();
+        
+            for (int w : G.adj(v)) {
+                if (!visited[w]) {
+                    edgeTo[w] = v;
+                    distTo[w] = distTo[v] + 1;
+                    visited[w] = true;
+                    q.add(w);
+                }
+            }
+        }
+    }
+
+    private void bfs(Digraph G, Iterable<Integer> sources) {
+    	LinkedList<Integer> q = new LinkedList<Integer>();
+    	
+        for (int s : sources) {
+            visited[s] = true;
+            distTo[s] = 0;
+            q.add(s);
+        }
+        
+        while (!q.isEmpty()) {
+        	int v = q.poll();
+
+        	for (int w : G.adj(v)) {
+                if (!visited[w]) {
+                    edgeTo[w] = v;
+                    distTo[w] = distTo[v] + 1;
+                    visited[w] = true;
+                    q.add(w);
+                }
+            }
+        }
+    }
+
+    public boolean hasPathTo(int v) {
+        validateVertex(v);
+        return visited[v];
+    }
+
+    public int distTo(int v) {
+        validateVertex(v);
+        return distTo[v];
+    }
+
+    public Iterable<Integer> pathTo(int v) {
+        validateVertex(v);
+
+        if (!hasPathTo(v)) 
+        	return null;
+        
+        Stack<Integer> path = new Stack<Integer>();
+        int x;
+        
+        for (x = v; distTo[x] != 0; x = edgeTo[x])
+            path.push(x);
+        
+        path.push(x);
+        return path;
+    }
+
+    private void validateVertex(int v) {
+        int V = visited.length;
+
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("Vertex " + v + " is not between 0 and " + (V - 1));
+    }
+
+    private void validateVertices(Iterable<Integer> vertices) {
+        if (vertices == null) 
+            throw new IllegalArgumentException("Argument is null");
+        
+        int V = visited.length;
+        
+        for (int v : vertices) 
+            if (v < 0 || v >= V) 
+                throw new IllegalArgumentException("Vertex " + v + " is not between 0 and " + (V - 1));
+    }
+}
