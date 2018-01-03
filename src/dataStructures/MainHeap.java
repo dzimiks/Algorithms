84e4acd7086d19a93293e41eca2f0701311a5920
dzimiks <vana997@gmail.com>
2018-01-03 14:13:52 +0100

    Add Max Priority Queue

--- src/dataStructures/MainHeap.java
+++ src/dataStructures/MainHeap.java
@@ -1,6 +1,8 @@
 package dataStructures;
 
+import dataStructures.heaps.IndexMaxPriorityQueue;
 import dataStructures.heaps.MaxHeap;
+import dataStructures.heaps.MaxPriorityQueue;
 import dataStructures.heaps.MinHeap;
 
 public class MainHeap {
@@ -41,5 +43,32 @@ public class MainHeap {
         System.out.println("\nThe Min Heap is:");
         minHeap.print();
         System.out.println("The Min val is " + minHeap.remove());
+        
+        // Max Priority Queue
+        MaxPriorityQueue<Integer> maxPQ = new MaxPriorityQueue<>();
+
+        maxPQ.insert(5);
+        maxPQ.insert(3);
+        maxPQ.insert(17);
+        maxPQ.insert(10);
+        maxPQ.insert(84);
+        maxPQ.insert(19);
+        maxPQ.insert(6);
+        maxPQ.insert(22);
+        maxPQ.insert(9);
+        
+        System.out.println("\nMax Priority Queue\n");
+        System.out.println("Max element is: " + maxPQ.delMax() + ", size of maxPQ: " + maxPQ.size());
+        
+        // Index Max Priority Queue
+        System.out.println("\nIndex Max Priority Queue\n");
+        String[] strings = {"it", "was", "the", "best", "of", "times", "it", "was", "the", "worst"};
+        IndexMaxPriorityQueue<String> indexMaxPQ = new IndexMaxPriorityQueue<>(strings.length);
+        
+        for (int i = 0; i < strings.length; i++)
+        	indexMaxPQ.insert(i, strings[i]);
+        
+        for (int i : indexMaxPQ)
+        	System.out.println(i + " " + strings[i]);
 	}
 }
